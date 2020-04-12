eb3315bb8657f6813db2670df696d9009ba5b854
A30004479 <A30004479@agl.com.au>
2020-04-12 23:25:31 +1000

    initial

---
+++ WebProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VirtualCoach.Fitness.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
